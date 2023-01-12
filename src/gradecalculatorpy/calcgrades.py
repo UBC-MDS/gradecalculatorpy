ce3cf04ef4e879bad373c8004fac15ab7a91abbf
shlrley <shirley_zh@icloud.com>
2023-01-12 07:04:11 -0800

    created calcgrades.py (function 3) and added some docstrings

---
+++ src/gradecalculatorpy/calcgrades.py
@@ -0,0 +1,47 @@
+# import pandas as pd
+
+#def load_course(input_file):
+    """Loads a .csv file containing information about the course and saves it in a pandas dataframe.
+    Parameters
+    ----------
+    input_file : str
+        Path to the .csv file.
+    Returns
+    -------
+    DataFrame
+        A pandas dataframe containing the information from the .csv file.
+    Examples
+    --------
+    >>> load_course("DSCI524.csv")
+    """
+    
+### MAYBE NOT NECESSARY:
+#def calc_finalgrade():
+    """Calculates the final grade of the course, given that all assignments in 'course.csv' have a grade.
+    Parameters
+    ----------
+    ...
+        ...
+    Returns
+    -------
+    ...
+        ...
+    Examples
+    --------
+    >>> ...
+    """
+    
+#def calc_gradesneeded():
+    """Calculates the grades for an assignment(s) needed in order to reach a goal grade for the course.
+    Parameters
+    ----------
+    ...
+        ...
+    Returns
+    -------
+    ...
+        ...
+    Examples
+    --------
+    >>> ...
+    """
