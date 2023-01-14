f22847e6188be0dcc14abedc2ac7eb5b10e2cbee
Yukun Zhang <ykzhang1211@g.ucla.edu>
2023-01-14 00:49:07 -0800

    add docstrings for predict_final()

---
+++ src/gradecalculatorpy/predict_final.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def predict_final(input_file, goal):
+    """
+    Calculate what grade the student needs to have for the final assignment in order to reach the goal for overall grade
+    and return the scores needed for the final assignment.
+    
+    Parameters
+    ----------
+    input_file : str
+        Path to read the saved .csv file as a string.
+    goal : double
+        The goal for your overall grade for this course.
+    
+    Returns
+    -------
+    final_score_needed : double
+    
+    Examples
+    --------
+    >>> predict_final('DSCI100.csv', 90.0)
+    """
