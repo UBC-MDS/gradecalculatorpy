0c05874bed7b8c22e248571815f6d41bceb315af
Yukun Zhang <ykzhang1211@g.ucla.edu>
2023-01-21 16:23:26 -0800

    modify input file path

--- tests/test_predict_final.py
+++ tests/test_predict_final.py
@@ -6,9 +6,9 @@ def test_predict_final():
     """ Create units test for the predict_final function."""
     
     # Set values
-    input_file_all = 'tests/dsci524_grades_all.csv'
-    input_file_miss = 'tests/dsci524_grades_miss.csv'
-    input_file_miss_two = 'tests/dsci524_grades_miss_two.csv'
+    input_file_all = '/tests/dsci524_grades_all.csv'
+    input_file_miss = '/tests/dsci524_grades_miss.csv'
+    input_file_miss_two = '/tests/dsci524_grades_miss_two.csv'
     goal = 80.0
     
     # Call the target fucntion and check if all assignment weight sum up to 100%
@@ -22,8 +22,8 @@ def test_predict_final():
     
     # Call the target fucntion and check if there if more than one NA value in the 'Grades (%)' column
     error_msg3 = "Please update all your assignment scores except the final."
-    assert predict_final(input_file_miss_two, goal) == error_msg2
-    assert predict_final(input_file_miss, goal) != error_msg2
+    assert predict_final(input_file_miss_two, goal) == error_msg3
+    assert predict_final(input_file_miss, goal) != error_msg3
     
     # Check the values calculated for the target fucntion
     assert predict_final(input_file_miss, goal) == 79.0
